e3eda3df1415548136c5e5337acf7f95612218fe
UlaNawrot <ula.nawrot@gmail.com>
2020-10-16 22:15:19 +0200

    I added aditional div in header, using JS

---
+++ script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    console.log('inside')
+    if (window.matchMedia("(min-width: 1000px)").matches) {
+        console.log('inside2')
+      var newDiv = document.createElement("div");
+      var parent = document.getElementById("head"); 
+      var my_div = document.getElementById("logo");
+      parent.insertBefore(newDiv, my_div);
+      console.log('inside3')
+    }
+}
+
+myFunction()
+
+/*function addElement()
+{
+  // tworzy nowy element div 
+  // i daje jego zawartość 
+  newDiv = document.createElement("div");
+  newDiv.innerHTML = "<h1>Hi there and greetings!</h1>";
+
+  // add the newly created element and it's content into the DOM
+  my_div = document.getElementById("logo");
+  document.body.insertBefore(newDiv, my_div);
+}*/
