f8a0273820d588d6821ee7e9e513e3c60089cf5f
UlaNawrot <ula.nawrot@gmail.com>
2020-10-22 14:23:43 +0200

    editing light-theme

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 function myFunction() {
-    console.log('inside')
-    if (window.matchMedia("(min-width: 1000px)").matches) {
-        console.log('inside2')
-      var newDiv = document.createElement("div");
-      var parent = document.getElementById("head"); 
-      var my_div = document.getElementById("logo");
-      
-      newDiv.setAttribute("id", "aditional");
-      
-      parent.insertBefore(newDiv, my_div);
-      console.log('inside3');
-      
-    }
+  console.log('inside')
+  if (window.matchMedia("(min-width: 1000px)").matches) {
+    console.log('inside2')
+    var newDiv = document.createElement("div");
+    var parent = document.getElementById("head");
+    var my_div = document.getElementById("logo");
+
+    newDiv.setAttribute("id", "aditional");
+
+    parent.insertBefore(newDiv, my_div);
+    console.log('inside3');
+
+  }
 }
 
 myFunction()
 
+const btn = document.querySelector(".btn-toggle");
+const theme = document.querySelector("#theme-link");
+btn.addEventListener("click", function () {
+  // Swap out the URL for the different stylesheets
+  if (theme.getAttribute("href") == "light-theme.css") {
+    theme.href = "dark-theme.css";
+  } else {
+    theme.href = "light-theme.css";
+  }
+});
+
 /*function addElement()
 {
-  // tworzy nowy element div 
-  // i daje jego zawartość 
+  // tworzy nowy element div
+  // i daje jego zawartość
   newDiv = document.createElement("div");
   newDiv.innerHTML = "<h1>Hi there and greetings!</h1>";
 
